0c3b4066cf36d14f8fa08eb8f235daabe63547ae
barttek.moj@gmail.com <barttek.moj@gmail.com>
2022-04-02 13:49:24 +0200

    Added settings struct

---
+++ 2Simple-Crypter/Core/Settings.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2Simple_Crypter.Core
+{
+    public struct Settings
+    {
+        // Build and Stub
+
+        public string buildDirectory;
+        public string payloadName;
+        public string resourceName;
+        public string decryptKey;
+        public string stubResources;
+        public string encryptedPayload;
+
+        // Startup
+
+        public string fileName;
+        public string specialDir;
+        public string regeditName;
+        public string schtasksName;
+        public string folderName;
+        public bool doRegedit;
+        public bool doSchtasks;
+        public bool doInstall;
+
+        // Assembly cloner
+        // In maintence
+    }
+}
