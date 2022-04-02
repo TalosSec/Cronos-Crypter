08f1037bdb5c0d91fed41d85c18ee9378a0e4856
barttek.moj@gmail.com <barttek.moj@gmail.com>
2022-04-02 13:51:17 +0200

    Added assembly settings

---
+++ 2Simple-Crypter/Core/Assembly.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2Simple_Crypter.Core
+{
+    public struct Assembly
+    {
+        // Assembly settings
+
+        public string iconPath;
+        public string assemblyTitle;
+        public string assemblyDescription;
+        public string assemblyProductName;
+        public string assemblyCompany;
+        public string assemblyVersion;
+        public string assemblyCopyright;
+        public bool usingAssembly;
+    }
+}
